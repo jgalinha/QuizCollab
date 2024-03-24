00e0669e9a68277e42c546cb31b3ed855696e34c
José Galinha <j.b.galinha@gmail.com>
2024-03-24 16:53:53 +0000

    Update User Class
    Signed-off-by: José Galinha <j.b.galinha@gmail.com>

--- QuizCollab.Domain/Users/User.cs
+++ QuizCollab.Domain/Users/User.cs
@@ -2,15 +2,19 @@
 
 namespace QuizCollab.Domain.Users;
 
-public sealed class User : Entity
+public sealed class User(
+    Guid id,
+    Username username,
+    Email email,
+    HashedPassword hashedPassword,
+    DateTime? lastLogin,
+    bool isSuperUser = false)
+    : Entity(id)
 {
-    public User(Guid id) : base(id)
-    {
-    }
-
-    public Username Username { get; }
-    public Email Email { get; }
-    public HashedPassword HashedPassword { get; }
-    public bool IsSuperUser { get; }
-    public DateTime? LastLogin { get; }
+    public Username Username { get; } = username;
+    public Email Email { get; } = email;
+    public HashedPassword HashedPassword { get; } = hashedPassword;
+    public bool IsSuperUser { get; } = isSuperUser;
+    public DateTime CreatedAt { get; } = DateTime.UtcNow;
+    public DateTime? LastLogin { get; } = lastLogin;
 }
